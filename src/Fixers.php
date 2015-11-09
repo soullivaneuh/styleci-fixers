128f22ef84b9367011117895c08f73e267943e69
Sullivan SENECHAL <soullivaneuh@gmail.com>
2015-11-10 00:01:45 +0100

    Fixers v0.1.0-alpha

---
+++ src/Fixers.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace SLLH\StyleCIFixers;
+
+/**
+ * This class was auto-generated from StyleCI/Config repository.
+ *
+ * @link https://github.com/StyleCI/Config/blob/master/src/Config.php
+ */
+class Fixers
+{
+    public static function getPresets()
+    {
+        return array(
+        );
+    }
+}
